7983c0f6055784084b6bccff41e9cbaf275bca80
Connor <164671442+ConnorC3@users.noreply.github.com>
2026-02-22 01:27:37 -0500

    Remove commented out code

--- backend/app/core/algebra.py
+++ backend/app/core/algebra.py
@@ -22,23 +22,3 @@ def _equations_equivalent(eq1: Eq, eq2: Eq) -> bool:
         raise ValueError(f"Only single-variable algebraic expressions supported.")
     x = syms[0]
     return set(solve(eq1, x)) == set(solve(eq2, x))
-
-# def equations_equal(prev: Eq, curr: Eq) -> bool:
-#     # Get all symbols
-#     symbols_set = list(prev.free_symbols.union(curr.free_symbols))
-
-#     # Handle constants
-#     if not symbols_set:
-#         return simplify(prev.lhs - prev.rhs) == simplify(curr.lhs - curr.rhs)
-
-#     if len(symbols_set) != 1:
-#         raise ValueError("Only single-variable equations supported here")
-
-#     x = symbols_set[0]
-
-#     # Solve each equation for x
-#     sol_prev = solve(prev, x)
-#     sol_curr = solve(curr, x)
-
-#     # Compare sets ignoring order
-#     return set(sol_prev) == set(sol_curr)
